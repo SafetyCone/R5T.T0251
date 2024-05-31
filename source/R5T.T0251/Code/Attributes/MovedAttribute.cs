6f22eed5a282978c5ed6ba02a9ccb2cccf49b15f
davidcoats <david@getsafetycone.com>
2024-05-30 22:25:47 -0700

    Interim changes.

---
+++ source/R5T.T0251/Code/Attributes/MovedAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0251
+{
+    /// <summary>
+    /// Similar to the <see cref="ObsoleteAttribute"/>, but without the Visual Studio built-in warning.
+    /// </summary>
+    /// <remarks>
+    /// Allowed on all application elements.
+    /// Multiple are allowed (although discouraged).
+    /// Inherited is false; just because the base class has moved doesn't mean any derived classes have.
+    /// </remarks>
+    [UtilityTypeMarker]
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    public class MovedAttribute : Attribute
+    {
+        public string Message { get; set; }
+
+
+        public MovedAttribute(
+            string message)
+        {
+            this.Message = message;
+        }
+    }
+}
